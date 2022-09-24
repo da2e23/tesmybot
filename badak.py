a86ea00d6588e81f334783ffacf2cae342b4761d
da2e23 <96852510+da2e23@users.noreply.github.com>
2022-09-25 03:07:00 +0900

    Update badak.py

--- badak.py
+++ badak.py
@@ -394,7 +394,7 @@ async def coin_price(interaction: nextcord.Interaction,
     print(response_c.json())
     trade_date = response_c.json()[0]['trade_date']
     trade_price = response_c.json()[0]['trade_price']
-    embed = discord.Embed(title=coin_name ,description='Upbit 시세', color=0x3498db)
+    embed = discord.Embed(title=coin_name+'(in Upbit)' ,description='', color=0x3498db)
     embed.add_field(name="거래 일시", value=trade_date, inline=False)
     embed.add_field(name="거래 가격", value=format(trade_price,','), inline=False)
     embed.set_footer(text="Honey Bottle🍯 | Badak")
